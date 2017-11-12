12fdd83d59c5aec375602dc07bd535fb2203aff2
fizhy37 <fis15028@byui.edu>
2017-11-11 21:37:22 -0700

    fixing up app

--- index.js
+++ index.js
@@ -8,80 +8,73 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
-app.get('/math', function(request, response) {
+app.get('/getRate', function(request, response) {
+  console.dir('test');
   var result = 0;
   var base = 0;
-  //get the operation....
+  //get the package type....
   var package = request.query.package;
-  //get the numbers..
-  // var numTEST = request.query.number1; //this one is said to be preferred. 
+  //get the numbers.. 
   var weight = request.query.weight;
   var weight = parseInt(weight);
   var rounded = Math.ceil(weight);
-  // console.log(num1);
-  // console.log(num2);
-  // console.log(operation);
-  // console.log(numTEST);
-  //perform the operation  
+
+  //perform the calculation  
   if (package == 'stamped') {
     base = .28;
-    result = base + rounded * .21;
+    result = Math.round(base + rounded * .21, 2);
   } else if (package == 'metered') {
     base = .25;
-    result = base + rounded * .21;
+    result = Math.round(base + rounded * .21, 2);
   } else if (package == 'flat') {
     base = .77;
-    result = base + rounded * .21;
+    result = Math.round(base + rounded * .21, 2);
   } else if (package == 'parcel') {
     if (rounded < 5) {
       result = 3.00;
     } else {
       base = 3.00;
       rounded = rounded - 4;
-      result = base + rounded * .16;
+      result = Math.round(base + rounded * .16, 2);
     }
   } else {
-    result = 40000;
+    result = 0;
   }
   //console the output
   console.log(result);
   response.render('pages/results', { result: result });
 });
-app.get('/math_service', function(request, response) {
+app.get('/calculateRate', function(request, response) {
   console.dir('test');
   var result = 0;
   var base = 0;
-  //get the operation....
+  //get the package type....
   var package = request.query.package;
-  //get the numbers..
-  // var numTEST = request.query.number1; //this one is said to be preferred. 
+  //get the numbers.. 
   var weight = request.query.weight;
   var weight = parseInt(weight);
   var rounded = Math.ceil(weight);
-  // console.log(num1);
-  // console.log(num2);
-  // console.log(operation);
-  // console.log(numTEST);
-  //perform the operation  
+
+  //perform the calculation  
   if (package == 'stamped') {
     base = .28;
-    result = base + rounded * .21;
+    result = Math.round(base + rounded * .21, 2);
   } else if (package == 'metered') {
     base = .25;
-    result = base + rounded * .21;
+    result = Math.round(base + rounded * .21, 2);
   } else if (package == 'flat') {
     base = .77;
-    result = base + rounded * .21;
+    result = Math.round(base + rounded * .21, 2);
   } else if (package == 'parcel') {
     if (rounded < 5) {
       result = 3.00;
     } else {
       base = 3.00;
       rounded = rounded - 4;
-      result = base + rounded * .16;
+      result = Math.round(base + rounded * .16, 2);
     }
   } else {
-    result = 40000;
+    result = 0;
   }
   //console the output
   console.log(result);
